e359e1dab8cd02d817ee86d22b9bf33938d1f5a2
Jordan Gottlieb <109196211+jdgott67@users.noreply.github.com>
2024-11-21 16:40:50 -0800

    Add files via upload

--- src/pages/Admin/Admin_Landing.tsx
+++ src/pages/Admin/Admin_Landing.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Landing_Header from "../../components/Global/Landing_Header"
+import Dashboard_Bar from "../../components/Global/Dashboard_Bar"
 
 const Admin_Landing: React.FC = () => {
   return (
     <div>
+      <Landing_Header/>
+      <Dashboard_Bar/>
       <h1>Admin Home</h1>
       <p>Welcome to your dashboard!</p>
     </div>
