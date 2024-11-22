e8ab7d6dda5759dedc1f722ab4daab8c1a7b0397
Jordan Gottlieb <109196211+jdgott67@users.noreply.github.com>
2024-11-21 16:41:47 -0800

    Update Employee_Landing.tsx

--- src/pages/Employee/Employee_Landing.tsx
+++ src/pages/Employee/Employee_Landing.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Landing_Header from "../../components/Global/Landing_Header"
+import Dashboard_Bar from "../../components/Global/Dashboard_Bar"
 
 const Employee_Landing: React.FC = () => {
   return (
     <div>
+      <Landing_Header/>
+      <Dashboard_Bar/>
       <h1>Employee Home</h1>
       <p>Welcome to your dashboard!</p>
     </div>
